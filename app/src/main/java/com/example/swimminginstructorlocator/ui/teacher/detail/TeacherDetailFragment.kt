1e07e4ee4580fa4d9efc7ce8c6ab3014e44533c8
Tan Dung <tandung12072002@gmail.com>
2023-10-26 15:59:46 +0700

    navigate

--- app/src/main/java/com/example/swimminginstructorlocator/ui/teacher/detail/TeacherDetailFragment.kt
+++ app/src/main/java/com/example/swimminginstructorlocator/ui/teacher/detail/TeacherDetailFragment.kt
@@ -6,18 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.swimminginstructorlocator.R
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+import com.example.swimminginstructorlocator.databinding.FragmentTeacherDetailBinding
+import com.example.swimminginstructorlocator.utils.base.BaseViewBindingFragment
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TeacherDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TeacherDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class TeacherDetailFragment : BaseViewBindingFragment<FragmentTeacherDetailBinding>() {
     private var param1: String? = null
     private var param2: String? = null
 
@@ -29,6 +24,19 @@ class TeacherDetailFragment : Fragment() {
         }
     }
 
+    override fun createBindingFragment(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentTeacherDetailBinding {
+        return FragmentTeacherDetailBinding.inflate(inflater, container, false)
+    }
+
+    override fun initData() {
+    }
+
+    override fun initView() {
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -36,15 +44,6 @@ class TeacherDetailFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TeacherDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic fun newInstance(param1: String, param2: String) =
                 TeacherDetailFragment().apply {
                     arguments = Bundle().apply {
--- app/src/main/java/com/example/swimminginstructorlocator/ui/teacher/search/SearchTeacherFragment.kt
+++ app/src/main/java/com/example/swimminginstructorlocator/ui/teacher/search/SearchTeacherFragment.kt
@@ -1,23 +1,15 @@
 package com.example.swimminginstructorlocator.ui.teacher.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import com.example.swimminginstructorlocator.R
+import com.example.swimminginstructorlocator.databinding.FragmentSearchTeacherBinding
+import com.example.swimminginstructorlocator.utils.base.BaseViewBindingFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchTeacherFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SearchTeacherFragment : Fragment() {
+class SearchTeacherFragment : BaseViewBindingFragment<FragmentSearchTeacherBinding>() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -30,24 +22,20 @@ class SearchTeacherFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_teacher, container, false)
+    override fun createBindingFragment(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentSearchTeacherBinding {
+        return FragmentSearchTeacherBinding.inflate(inflater, container, false)
+    }
+
+    override fun initData() {
+    }
+
+    override fun initView() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchTeacherFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             SearchTeacherFragment().apply {
