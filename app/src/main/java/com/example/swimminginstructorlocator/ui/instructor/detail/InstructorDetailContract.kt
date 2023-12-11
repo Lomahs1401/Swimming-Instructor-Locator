6c4104c38c555b2aaa8beedf9f1b985a9ed9b42b
Lomahs1401 <lelong.ll32@gmail.com>
2023-12-11 15:43:07 +0700

    Update instructor detail UI and add check manage account logged yet

--- app/src/main/java/com/example/swimminginstructorlocator/ui/instructor/detail/InstructorDetailContract.kt
+++ app/src/main/java/com/example/swimminginstructorlocator/ui/instructor/detail/InstructorDetailContract.kt
@@ -1,5 +1,7 @@
 package com.example.swimminginstructorlocator.ui.instructor.detail
 
+import com.example.swimminginstructorlocator.data.model.Course
+import com.example.swimminginstructorlocator.data.model.CourseDetail
 import com.example.swimminginstructorlocator.data.model.InstructorDetail
 import com.example.swimminginstructorlocator.listener.OnInstructorDetailListener
 import com.example.swimminginstructorlocator.utils.base.BasePresenter
@@ -10,6 +12,8 @@ class InstructorDetailContract {
      * View
      */
     interface View {
+        fun onGetListCourse(listCourse: MutableList<Course>)
+        fun onGetCourseDetail(course: CourseDetail)
         fun onError(exception: Exception?)
     }
 
@@ -17,5 +21,7 @@ class InstructorDetailContract {
      * Presenter
      */
     interface Presenter : BasePresenter<View> {
+        fun getListCourse(instructorId: String)
+        fun getCourseDetail(id: String)
     }
 }
--- app/src/main/java/com/example/swimminginstructorlocator/ui/instructor/detail/InstructorDetailPresenter.kt
+++ app/src/main/java/com/example/swimminginstructorlocator/ui/instructor/detail/InstructorDetailPresenter.kt
@@ -1,13 +1,15 @@
 package com.example.swimminginstructorlocator.ui.instructor.detail
 
-import com.example.swimminginstructorlocator.data.model.InstructorDetail
+import com.example.swimminginstructorlocator.data.model.Course
+import com.example.swimminginstructorlocator.data.model.CourseDetail
+import com.example.swimminginstructorlocator.data.service.impl.CourseServiceImpl
 import com.example.swimminginstructorlocator.data.service.impl.InstructorServiceImpl
-import com.example.swimminginstructorlocator.listener.OnInstructorDetailListener
 import com.example.swimminginstructorlocator.listener.OnResultListener
 import java.lang.Exception
 
 class InstructorDetailPresenter(
-    private val instructorService: InstructorServiceImpl
+    private val instructorService: InstructorServiceImpl,
+    private val courseService: CourseServiceImpl
 ) : InstructorDetailContract.Presenter {
 
     private var view: InstructorDetailContract.View? = null
@@ -22,5 +24,35 @@ class InstructorDetailPresenter(
 
     override fun setView(view: InstructorDetailContract.View?) {
         this.view = view
+    }
+
+    override fun getListCourse(instructorId: String) {
+        instructorService.getListCourse(
+            instructorId,
+            object : OnResultListener<MutableList<Course>> {
+                override fun onSuccess(dataResult: MutableList<Course>) {
+                    view?.onGetListCourse(dataResult)
+                }
+
+                override fun onError(exception: Exception?) {
+                    view?.onError(exception)
+                }
+            }
+        )
+    }
+
+    override fun getCourseDetail(id: String) {
+        courseService.getCourseDetail(
+            id,
+            object : OnResultListener<CourseDetail> {
+                override fun onSuccess(dataResult: CourseDetail) {
+                    view?.onGetCourseDetail(dataResult)
+                }
+
+                override fun onError(exception: Exception?) {
+                    view?.onError(exception)
+                }
+            }
+        )
     }
 }
